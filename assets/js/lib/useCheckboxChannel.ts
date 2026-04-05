2ebbdde68efed8c521794c4545ff59b60ef7e93e
hubcio2115 <kowalski.hubercik1@gmail.com>
2026-04-05 22:17:06 +0200

    added helper hook for connecting and mounting Sockets

---
+++ assets/js/lib/useCheckboxChannel.ts
@@ -0,0 +1,35 @@
+import { Channel, Socket } from "phoenix";
+import { useEffect, useRef } from "react";
+
+export function useCheckboxChannel(...listeners: Parameters<Channel["on"]>[]) {
+  const channelRef = useRef<Channel | null>(null);
+
+  useEffect(() => {
+    const socket = new Socket("/socket");
+    socket.connect();
+
+    const channel = socket.channel("room:checkboxes");
+
+    channel
+      .join()
+      .receive("ok", (resp) => {
+        console.log(resp);
+      })
+      .receive("error", (err) => {
+        console.error(err);
+      });
+
+    for (const listener of listeners) {
+      channel.on(...listener);
+    }
+
+    channelRef.current = channel;
+
+    return () => {
+      channel.leave();
+      socket.disconnect();
+    };
+  }, []);
+
+  return channelRef;
+}
