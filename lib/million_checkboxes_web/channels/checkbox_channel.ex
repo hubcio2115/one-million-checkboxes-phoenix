367626df843e3943c2f21adcd1e073641f3cf4f8
hubcio2115 <kowalski.hubercik1@gmail.com>
2026-04-05 22:16:27 +0200

    created checkboxes channel

---
+++ lib/million_checkboxes_web/channels/checkbox_channel.ex
@@ -0,0 +1,22 @@
+defmodule MillionCheckboxesWeb.CheckboxChannel do
+  alias MillionCheckboxes.BoolStore
+  use Phoenix.Channel
+
+  def join("room:checkboxes", _message, socket) do
+    {:ok, socket}
+  end
+
+  def join("room:" <> _private_room_id, _params, _socket) do
+    {:error, %{reason: "unauthorized"}}
+  end
+
+  def handle_in("checked", %{"index" => index, "checked" => checked}, socket) do
+    with :ok <- BoolStore.set(index, checked) do
+      broadcast!(socket, "checked", %{index: index, checked: checked})
+      {:noreply, socket}
+    else
+      {:error, reason} ->
+        {:reply, {:error, %{message: reason}}, socket}
+    end
+  end
+end
